1663886d9abd851b849e82285ab7a82cb132e8d7
Saif Medhat <seifmedhatfarouk@gmail.com>
2023-12-27 21:24:14 +0200

    added Missing Coin Sum

---
+++ Sorting and Searching/Missing Coin Sum.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>    
+#ifndef ONLINE_JUDGE
+#include <deb.h>
+#else
+#define deb(x)
+#endif
+using namespace std;
+#define ll long long
+#define oo 1e18 
+#define int ll
+#define mod 1000000007
+#define f first
+#define s second
+#define popcnt __builtin_popcountll
+#define clr(v, d) memset(v,d,sizeof(v))
+#define ii pair<int,int>
+template<typename T> bool chkmin(T &a, T b){return (b < a) ? a = b, 1 : 0;}
+template<typename T> bool chkmax(T &a, T b){return (b > a) ? a = b, 1 : 0;}
+
+int dx[] = {0 , 1 , 0 , -1};
+int dy[] = {1 , 0 , -1 , 0};
+const int N = 1e6+17;
+int n;
+
+std::vector<int> v;
+
+void solve(){
+	cin >> n;
+	v.resize(n);
+	for(int &i : v)
+		cin >> i;
+	sort(v.begin(), v.end());
+
+	int s = 1;	
+	for(int i=0; i<n; i++){
+		if(v[i] > s){
+			cout << s;
+			return;
+		}
+		s += v[i];
+	}
+	cout << s;
+}	
+  
+main() {
+    cin.tie(0);cin.sync_with_stdio(0);
+    cout.tie(0);cout.sync_with_stdio(0);
+   //  freopen("ladder.in", "r", stdin);
+	 	// freopen("ladder.out", "w", stdout);  
+    int t = 1;
+   	//cin >> t;
+    
+     for (int L = 0; L < t; L++){
+       solve();    
+     }
+} 
