f25c2a90089e7bc0e505356f097eaa687fb53bc6
Saif Medhat <seifmedhatfarouk@gmail.com>
2023-12-28 09:51:59 +0200

    added Collecting Numbers

---
+++ Sorting and Searching/Collecting Numbers.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>    
+#ifndef ONLINE_JUDGE
+#include <deb.h>
+#else
+#define deb(x)
+#endif
+using namespace std;
+#define ll long long
+#define oo 1e18 
+#define int ll
+#define mod 1000000007
+#define f first
+#define s second
+#define popcnt __builtin_popcountll
+#define clr(v, d) memset(v,d,sizeof(v))
+#define ii pair<int,int>
+template<typename T> bool chkmin(T &a, T b){return (b < a) ? a = b, 1 : 0;}
+template<typename T> bool chkmax(T &a, T b){return (b > a) ? a = b, 1 : 0;}
+
+int dx[] = {0 , 1 , 0 , -1};
+int dy[] = {1 , 0 , -1 , 0};
+const int N = 1e6+17;
+
+int n , ar[N] , idx[N];
+
+void solve(){
+	cin >> n;
+	for(int i=1 ;i<=n; i++){ 
+		cin >> ar[i];
+		idx[ar[i]] = i;
+	}
+	
+	int ans = 0;
+	for(int i=2; i<=n; i++) {
+		ans += idx[i] < idx[i - 1];
+	}
+
+	cout << ans + 1 << '\n';
+}	
+  
+main() {
+    cin.tie(0);cin.sync_with_stdio(0);
+    cout.tie(0);cout.sync_with_stdio(0);
+   //  freopen("ladder.in", "r", stdin);
+	 	// freopen("ladder.out", "w", stdout);  
+    int t = 1;
+   	//cin >> t;
+    
+     for (int L = 0; L < t; L++){
+       solve();    
+     }
+} 
