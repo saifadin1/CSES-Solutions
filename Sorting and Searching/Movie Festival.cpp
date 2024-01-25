2d3b7474d11775e478d8db6be6d738caf84ed077
Saif Medhat <seifmedhatfarouk@gmail.com>
2024-01-25 17:12:18 +0200

    added Restaurant Customers

--- Sorting and Searching/Movie Festival.cpp
+++ Sorting and Searching/Movie Festival.cpp
@@ -16,25 +16,29 @@ using namespace std;
 #define ii pair<int,int>
 template<typename T> bool chkmin(T &a, T b){return (b < a) ? a = b, 1 : 0;}
 template<typename T> bool chkmax(T &a, T b){return (b > a) ? a = b, 1 : 0;}
-
+ 
 int dx[] = {0 , 1 , 0 , -1};
 int dy[] = {1 , 0 , -1 , 0};
 const int N = 1e6+17;
-int n , k;
-
+int n;
+ 
 void solve(){
-	cin >> n >> k;
-	map<int,int> mp;
-	for(int i=0; i<n; i++){
-		int in;
-		cin >> in;
-		if(mp[k - in]){
-			cout << mp[k - in] + 1 << ' ' << i+1;
-			return;
-		}
-		mp[in] = i;
+	cin >> n;
+	std::vector<ii> v(n);
+	for(ii &i : v){
+		cin >> i.s >> i.f;
 	}
-	cout << "IMPOSSIBLE";
+	sort(v.begin(), v.end());
+ 
+	int nowEnd = v[0].f , ans = 1;
+	for(int i=1; i<n; i++){
+		if(v[i].s >= nowEnd){
+			ans ++;
+			nowEnd = v[i].f;
+		}
+	}
+ 
+	cout << ans;
 }	
   
 main() {
@@ -48,4 +52,4 @@ main() {
      for (int L = 0; L < t; L++){
        solve();    
      }
-} 
+}
--- Sorting and Searching/Restaurant Customers.cpp
+++ Sorting and Searching/Restaurant Customers.cpp
@@ -16,28 +16,52 @@ using namespace std;
 #define ii pair<int,int>
 template<typename T> bool chkmin(T &a, T b){return (b < a) ? a = b, 1 : 0;}
 template<typename T> bool chkmax(T &a, T b){return (b > a) ? a = b, 1 : 0;}
-
+ 
 int dx[] = {0 , 1 , 0 , -1};
 int dy[] = {1 , 0 , -1 , 0};
 const int N = 1e6+17;
 int n;
-
+ 
 void solve(){
 	cin >> n;
 	std::vector<ii> v(n);
-	for(ii &i : v){
-		cin >> i.s >> i.f;
+	vector<int> flat;
+	for(ii &i : v)
+		cin >> i.f >> i.s;
+ 
+	for(int i=0; i<n; i++){
+		flat.push_back(v[i].f);
+		flat.push_back(v[i].s);
 	}
-	sort(v.begin(), v.end());
-
-	int nowEnd = v[0].f , ans = 1;
-	for(int i=1; i<n; i++){
-		if(v[i].s >= nowEnd){
-			ans ++;
-			nowEnd = v[i].f;
-		}
+	sort(flat.begin(), flat.end());
+ 	
+	// compress
+	map<int,int> idx;
+	for(int i=0; i<n*2; i++){
+		idx[flat[i]] = i;
 	}
-
+ 
+	for(int i=0; i<n; i++){
+		v[i].f = idx[v[i].f];
+		v[i].s = idx[v[i].s];
+	}
+ 
+ 
+	vector<int> pre(n * 2 + 1);
+	for(int i=0; i<n; i++){
+		pre[v[i].f] ++;
+		pre[v[i].s + 1] --;
+	}
+ 
+	for(int i=1; i<2*n; i++){
+		pre[i] += pre[i - 1];
+	}
+ 
+	int ans = 1;
+	for(int i=0; i<2*n; i++){
+		chkmax(ans , pre[i]);
+	}
+ 
 	cout << ans;
 }	
   
