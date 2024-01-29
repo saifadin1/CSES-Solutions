5b76a7db3d1cd7e76a70a9d8e9e2dce7d0d47ade
Saif Medhat <seifmedhatfarouk@gmail.com>
2024-01-29 02:09:35 +0200

    added Building Teams

---
+++ Graph Algorithms/Building Teams.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>    
+#ifndef ONLINE_JUDGE
+#include <deb.h>
+#else
+#define deb(x)
+#endif
+using namespace std;
+#define ll long long
+#define oo 2e9 + 1 
+#define int ll
+#define mod 1000000007
+#define f first
+#define s second
+#define popcnt __builtin_popcountll
+#define clr(v, d) memset(v,d,sizeof(v))
+#define ii pair<int,int>
+template<typename T> bool chkmin(T &a, T b){return (b < a) ? a = b, 1 : 0;}
+template<typename T> bool chkmax(T &a, T b){return (b > a) ? a = b, 1 : 0;}
+
+int dx[8]={1,0,-1,0,1,1,-1,-1};
+int dy[8]={0,-1,0,1,1,-1,-1,1};
+
+
+const int N = 2e5 + 123;
+
+void solve(){
+  int n , m;
+  cin >> n >> m;
+  std::vector<std::vector<int>> graph(n + 1);
+  std::vector<int> vis(n + 1);
+  std::vector<int> col(n + 1);
+  for(int i=0; i<m; i++){
+    int u , v;
+    cin >> u >> v;
+    graph[u].push_back(v);
+    graph[v].push_back(u);
+  }
+  function<bool(int)> bfs = [&](int u){
+    queue<int>q;
+    q.push(u);
+    while(q.size()){
+      int cur = q.front();
+      q.pop();
+      for(int v : graph[cur]){
+        if(!vis[v]){
+          vis[v] = 1;
+          col[v] = col[cur] ^ 1;
+          q.push(v);
+        } else if(col[v] == col[cur]){
+          return false;
+        }
+      }
+    }
+    return true;
+  };
+  bool ok = true;
+  for(int i=1; i<=n; i++){
+    if(!vis[i]) ok &= bfs(i);
+  }
+  if(!ok){
+    cout << "IMPOSSIBLE";
+    return;
+  }
+  for(int i=1; i<=n; i++){
+    cout << col[i] + 1 << ' ' ;
+  }
+}
+
+
+main() {
+    cin.tie(0);cin.sync_with_stdio(0);
+    cout.tie(0);cout.sync_with_stdio(0);
+   //  freopen("ladder.in", "r", stdin);
+    // freopen("ladder.out", "w", stdout);  
+    int t = 1;
+  //  cin >> t;
+
+     for (int L = 0; L < t; L++){
+      //cout << "Case " << L + 1 << ": ";
+       solve();    
+     }
+} 
