f3a92cb0c4eace25c5703aacd0c67c33ca7afd8c
Saif Medhat <seifmedhatfarouk@gmail.com>
2024-02-19 02:36:48 +0200

    added Playlist

---
+++ Sorting and Searching/Playlist.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>    
+#ifndef ONLINE_JUDGE
+#include <deb.h>
+#else
+#define deb(x)
+#endif
+using namespace std;
+#define oo 1e18
+#define int long long
+#define mod 1000000007
+#define f first
+#define s second
+#define clr(v, d) memset(v,d,sizeof(v))
+#define ii pair<int,int>
+template<typename T> bool chkmin(T &a, T b){return (b < a) ? a = b, 1 : 0;}
+template<typename T> bool chkmax(T &a, T b){return (b > a) ? a = b, 1 : 0;}
+
+int dx[8]={1,0,-1,0,1,1,-1,-1};
+int dy[8]={0,-1,0,1,1,-1,-1,1};
+  
+const int N = 2e5 + 123;
+
+void solve(){
+  int n;
+  cin >> n;
+  map<int,int> mp;
+  int ans = 1 , l=1;
+  for(int r=1; r<=n; r++){
+    int in;
+    cin >> in;
+    if(mp[in]){
+      chkmax(ans , r - l);
+      l = max(l , mp[in] + 1);
+      mp[in] = r;
+    } else {
+      chkmax(ans , r - l + 1);
+      mp[in] = r;
+    }
+  }
+  cout << ans;
+} 
+ 
+main() {
+  
+  cin.tie(0);cin.sync_with_stdio(0);
+  cout.tie(0);cout.sync_with_stdio(0);
+  // freopen("ladder.in", "r", stdin);
+  // freopen("ladder.out", "w", stdout);  
+  int t = 1;
+  //cin >> t;
+   
+
+
+   for (int L = 0; L < t; L++){
+    //cout << "Case " << L + 1 << ": ";
+     solve();    
+   }
+}
