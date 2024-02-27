dbcda78df1db626889f5a86fe4530d0708cf6114
Saif Medhat <seifmedhatfarouk@gmail.com>
2024-02-27 06:44:15 +0200

    added Subarray Sums II

---
+++ Sorting and Searching/Subarray Sums II.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>    
+#ifndef ONLINE_JUDGE
+#include <deb.h>
+#else
+#define deb(x)
+#endif
+using namespace std;
+#define oo 1e18
+#define int long long
+#define mod 1000000007
+#define f first
+#define s second
+#define clr(v, d) memset(v,d,sizeof(v))
+#define ii pair<int,int>
+template<typename T> bool chkmin(T &a, T b){return (b < a) ? a = b, 1 : 0;}
+template<typename T> bool chkmax(T &a, T b){return (b > a) ? a = b, 1 : 0;}
+
+int dx[8]={1,0,-1,0,1,1,-1,-1};
+int dy[8]={0,-1,0,1,1,-1,-1,1};
+  
+
+const double pi = 3.14159265359;
+
+const int N = 1e6 + 123;
+
+
+void solve(){ 
+  int ans = 0 , n , k;
+  cin >> n >> k;
+  map<int,int> mp;
+  mp[0] = 1;
+  int pre = 0;
+  while(n--){
+    int x;
+    cin >> x;
+    pre += x;
+    ans += mp[pre - k];
+    mp[pre]++;
+  }
+  cout << ans;
+} 
+
+
+ 
+main() {
+  
+  cin.tie(0);cin.sync_with_stdio(0);
+  cout.tie(0);cout.sync_with_stdio(0);
+  // freopen("ladder.in", "r", stdin);
+  // freopen("ladder.out", "w", stdout);  
+  int t = 1;
+  //cin >> t;
+
+
+
+
+   for (int L = 0; L < t; L++){
+    //cout << "Case " << L + 1 << ": ";
+     solve();    
+   }
+}
