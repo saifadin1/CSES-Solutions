e688c968abc2397e3c86872f4e861e0c00a00d24
Saif Medhat <seifmedhatfarouk@gmail.com>
2025-01-05 00:42:40 +0200

    Add Number Spiral

---
+++ Introductory Problems/Number Spiral.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#ifndef ONLINE_JUDGE
+// #include <deb.h>
+#else
+#define deb(x)
+#endif
+using namespace std;
+using namespace __gnu_pbds;
+#define int long long
+#define ii pair<int,int>
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+template<typename T> bool chkmin(T &a, T b){return (b < a) ? a = b, 1 : 0;}
+template<typename T> bool chkmax(T &a, T b){return (b > a) ? a = b, 1 : 0;}
+auto random_address = [] { char *p = new char; delete p; return (uint64_t) p; };
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count() * (random_address() | 1));
+const int mod = 998244353 , N = 2e5 + 9;
+
+
+void solve(){
+    int x , y;
+    cin >> x >> y;
+    if(x == y && x == 1){
+        cout << "1\n";
+        return;
+    }
+
+    int layer = max(x , y);
+    int index = min(x , y);
+    int lower = (layer - 1) * (layer - 1) + 1;
+    int upper = (layer) * (layer);
+
+
+    if(layer % 2 == 1){
+        if(y >= x){
+            cout << upper - index + 1 << '\n'; 
+        } else {
+            cout << lower + index - 1 << '\n';
+        }
+    } else {
+        if(y >= x){
+            cout << lower + index - 1 << '\n';
+        } else {
+            cout << upper - index + 1 << '\n'; 
+        }
+    }
+
+}
+
+
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
