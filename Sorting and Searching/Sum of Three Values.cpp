6481d75ff0a4b3f7cfe4ae9d241ca2a9075e7e57
Saif Medhat <seifmedhatfarouk@gmail.com>
2025-01-08 23:12:38 +0200

    Add Sum of Three Values.cpp

---
+++ Sorting and Searching/Sum of Three Values.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#ifndef ONLINE_JUDGE
+// #include <deb.h>
+#else
+#define deb(x)
+#endif
+using namespace std;
+using namespace __gnu_pbds;
+#define int long long
+#define ii pair<int,int>
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+
+template<typename T> bool chkmin(T &a, T b){return (b < a) ? a = b, 1 : 0;}
+template<typename T> bool chkmax(T &a, T b){return (b > a) ? a = b, 1 : 0;}
+auto random_address = [] { char *p = new char; delete p; return (uint64_t) p; };
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count() * (random_address() | 1));
+
+const int mod = 998244353 , N = 2e5 + 9;
+
+
+void solve() {
+    int n , k;
+    cin >> n >> k;
+    vector<pair<int,int>> a(n);
+    for(int i=0; i<n; i++) {
+        cin >> a[i].first;
+        a[i].second = i;   
+    }
+    sort(a.begin(), a.end());
+    for(int i=0; i<n; i++){
+        int need = k - a[i].first;
+        int l=0, r=n-1;
+        while(l < r) {
+            int curState = a[l].first + a[r].first;
+            if(l ^ i && r ^ i && curState == need) {
+                cout << a[i].second + 1 << " " << a[l].second + 1 << " " << a[r].second + 1;
+                return;
+            } else if(curState < need) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+    }
+
+    cout << "IMPOSSIBLE";
+}
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+}   
