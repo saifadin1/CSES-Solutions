76ab37a86f7623f2178102f5a230a834e791df8f
Saif Medhat <seifmedhatfarouk@gmail.com>
2025-01-20 20:47:06 +0200

    Add Increasing Subsequence.cpp

--- Dynamic Programming/Increasing Subsequence/Increasing Subsequence.cpp
+++ Dynamic Programming/Increasing Subsequence/Increasing Subsequence.cpp
@@ -17,25 +17,29 @@ template<typename T> bool chkmax(T &a, T b){return (b > a) ? a = b, 1 : 0;}
 auto random_address = [] { char *p = new char; delete p; return (uint64_t) p; };
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count() * (random_address() | 1));
 
-const int mod = 998244353 , N = 2e5 + 9;
+const int mod = 1e9 + 7 , N = 2e5 + 9;
 
+int mul(int a, int b) {
+    return (a * b) % mod;
+}
+
+int add(int a, int b) {
+    return (a + b) % mod;
+}
 
 void solve() {
     int n;
     cin >> n;
-    vector<int> piles;
-    for (int i=0; i<n; i++) {
-        int x;
-        cin >> x;
-        auto it = lower_bound(piles.begin(), piles.end(), x);
-        if (it == piles.end()) {
-            piles.push_back(x);
-        } else {
-            *it = x;
-        }
+    vector<vector<int>> dp(n + 1 , vector<int>(2));
+    dp[1][0] = dp[1][1] = 1;
+    for (int i=2; i<=n; i++) {
+        dp[i][1] = mul(2 , dp[i - 1][0]);
+        dp[i][1] = add(dp[i][1] , dp[i - 1][1]);
+        dp[i][0] = dp[i - 1][1];   
+        dp[i][0] = mul(dp[i][0] , 2);
     }
 
-    cout << piles.size();
+    cout << add(dp[n][0] , dp[n][1]);
 } 
 
 
@@ -49,7 +53,7 @@ main() {
     #endif
 
     int t = 1;
-    // cin >> t;
+    cin >> t;
     while (t--) {
         solve();
     }
