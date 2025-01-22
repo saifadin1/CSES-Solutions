ad9a86e598e3b241201f5ccbbf5720ac94c3914b
Saif Medhat <seifmedhatfarouk@gmail.com>
2025-01-22 05:20:15 +0200

    Add Road Construction.cpp

---
+++ Graph Algorithms/Road Construction/Road Construction.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#ifndef ONLINE_JUDGE
+// #include <deb.h>
+#else
+#define deb(x)
+#endif
+using namespace std;
+using namespace __gnu_pbds;
+#define int long long
+#define ii pair<int,int>
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+ 
+template<typename T> bool chkmin(T &a, T b){return (b < a) ? a = b, 1 : 0;}
+template<typename T> bool chkmax(T &a, T b){return (b > a) ? a = b, 1 : 0;}
+auto random_address = [] { char *p = new char; delete p; return (uint64_t) p; };
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count() * (random_address() | 1));
+ 
+const int mod = 1e9 + 7 , N = 1e6 + 9;
+
+class DSU {
+public:
+    std::vector<int> parent;
+    std::vector<int> rank;
+    int maxRank = 1;
+    int groups = 0;
+
+    DSU(int n) {
+        groups = n;
+        parent.resize(n);
+        rank.resize(n, 1);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i; 
+        }
+    }
+
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]); 
+        }
+        return parent[x];
+    }
+
+    void unite(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX != rootY) {
+            if (rank[rootX] >= rank[rootY]) {
+                parent[rootY] = rootX;
+                rank[rootX] += rank[rootY];
+            } else if (rank[rootX] < rank[rootY]) {
+                rank[rootY] += rank[rootX];
+                parent[rootX] = rootY;
+            }
+            chkmax(maxRank, max(rank[rootX], rank[rootY]));
+            groups--;
+        }
+    }
+
+    bool connected(int x, int y) {
+        return find(x) == find(y);
+    }
+};
+
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    DSU dsu(n);
+    for (int i=0; i<m; i++) {
+        int a, b;
+        cin >> a >> b;
+        a--, b--;
+        dsu.unite(a, b);
+        cout << dsu.groups << ' ' << dsu.maxRank << "\n";
+    }
+}
+
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+ 
+
+ 
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+} 
--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 | Topic         | Number Solved | Progress                                  |
 |-----------------------|---------------|-------------------------------------------|
 | Sorting and Searching | 19/35 | ![](https://geps.dev/progress/54) |
-| Graph Algorithms | 17/36 | ![](https://geps.dev/progress/47) |
+| Graph Algorithms | 18/36 | ![](https://geps.dev/progress/50) |
 | Introductory Problems | 14/19 | ![](https://geps.dev/progress/73) |
 | Dynamic Programming | 14/19 | ![](https://geps.dev/progress/73) |
 | Mathematics | 10/31 | ![](https://geps.dev/progress/32) |
@@ -15,5 +15,5 @@
 | Geometry | 0/7 | ![](https://geps.dev/progress/0) |
 | Advanced Techniques | 0/24 | ![](https://geps.dev/progress/0) |
 | Additional Problems | 0/77 | ![](https://geps.dev/progress/0) |
-| overall               | 88/300 | ![](https://geps.dev/progress/29) |
+| overall               | 89/300 | ![](https://geps.dev/progress/29) |
 
