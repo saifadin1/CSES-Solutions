73a1a83fed0991b6c7d170b8613fb58f0ceeb9c8
Saif Medhat <seifmedhatfarouk@gmail.com>
2025-01-22 04:07:28 +0200

    Add Coin Piles.cpp

---
+++ Introductory Problems/Coin Piles/Coin Piles.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#ifndef ONLINE_JUDGE
+// #include <deb.h>
+#else
+#define deb(x)
+#endif
+using namespace std;
+using namespace __gnu_pbds;
+#define int long long
+#define ii pair<int,int>
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+ 
+template<typename T> bool chkmin(T &a, T b){return (b < a) ? a = b, 1 : 0;}
+template<typename T> bool chkmax(T &a, T b){return (b > a) ? a = b, 1 : 0;}
+auto random_address = [] { char *p = new char; delete p; return (uint64_t) p; };
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count() * (random_address() | 1));
+ 
+const int mod = 1e9 + 7 , N = 1e6 + 9;
+
+void solve() {
+    int a , b;
+    cin >> a >> b;
+    if (a < b) swap(a, b);
+    if (b * 2 >= a && (a + b) % 3 == 0) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+} 
+
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+ 
+
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+} 
--- README.md
+++ README.md
@@ -6,8 +6,8 @@
 |-----------------------|---------------|-------------------------------------------|
 | Sorting and Searching | 19/35 | ![](https://geps.dev/progress/54) |
 | Graph Algorithms | 17/36 | ![](https://geps.dev/progress/47) |
+| Introductory Problems | 14/19 | ![](https://geps.dev/progress/73) |
 | Dynamic Programming | 14/19 | ![](https://geps.dev/progress/73) |
-| Introductory Problems | 13/19 | ![](https://geps.dev/progress/68) |
 | Mathematics | 10/31 | ![](https://geps.dev/progress/32) |
 | Range Queries | 9/19 | ![](https://geps.dev/progress/47) |
 | Tree Algorithms | 4/16 | ![](https://geps.dev/progress/25) |
@@ -15,5 +15,5 @@
 | Geometry | 0/7 | ![](https://geps.dev/progress/0) |
 | Advanced Techniques | 0/24 | ![](https://geps.dev/progress/0) |
 | Additional Problems | 0/77 | ![](https://geps.dev/progress/0) |
-| overall               | 87/300 | ![](https://geps.dev/progress/29) |
+| overall               | 88/300 | ![](https://geps.dev/progress/29) |
 
